d0e1ab6f6dc644e5378708ed651e06b8f2630ca0
Horst Egon <benedikt.maier@cern.ch>
2022-04-25 05:02:14 -0400

    eta-e bugfix

--- h5_gen/awkwardupuppi.py
+++ h5_gen/awkwardupuppi.py
@@ -4,13 +4,15 @@ import awkward as ak
 import numpy as np
 import uproot as uproot
 import smear_chargedhadrons as sch
+from awkward import Array
+from awkward.layout import ListOffsetArray64
 
 #np.random.seed(0)
 
 file = uproot.open(sys.argv[1])
 
 events = file['Delphes']
-nEvents = 1000
+nEvents = 100
 
 # read branches
 def read_branch(branchname):
@@ -30,18 +32,38 @@ gen_pfs_charge = read_branch('PileUpMix.Charge')
 gen_pfs_genvtx = read_branch('PileUpMix.GenVtxIdx')
 gen_pfs_genvtx = gen_pfs_genvtx + 1
 
+
+
+correct_zs = []
+
+gen_pfs_z_new = gen_vtx_z[gen_pfs_genvtx]
+gen_pfs_z = gen_pfs_z_new
+
+'''
+for e in range(len(gen_pfs_z)):
+    print(e)
+    for z in range(len(gen_pfs_z[e])):
+        correct_zs.append(gen_vtx_z[e][gen_pfs_genvtx[e][z]])
+
+gen_pfs_z_new = Array(ListOffsetArray64(gen_pfs_z.layout.offsets, Array(np.asarray(correct_zs)).layout))
+'''
+
+#print(gen_pfs_z_new[6])
+
+#print(len(gen_pfs_z_new), len(gen_pfs_z))
+#print(type(gen_pfs_z_new), type(gen_pfs_z))
+
 #print(gen_pfs_pt)
 
 
 
 # neutrino mask
-neutrino_mask = (np.abs(gen_pfs_pid) != 12) & (np.abs(gen_pfs_pid) != 14) & (np.abs(gen_pfs_pid) != 16)
+neutrino_mask = (np.abs(gen_pfs_pid) != 12) & (np.abs(gen_pfs_pid) != 14) & (np.abs(gen_pfs_pid) != 16) & (np.abs(gen_pfs_eta) < 5.0)
 def filter_neutrinos(arr):
     newarr = arr[neutrino_mask]
     return newarr
 
 gen_pfs_pt = filter_neutrinos(gen_pfs_pt)
-#print(gen_pfs_pt)
 
 gen_pfs_eta = filter_neutrinos(gen_pfs_eta)
 gen_pfs_phi = filter_neutrinos(gen_pfs_phi)
@@ -59,7 +81,7 @@ def smear_em(gen_pfs_pt,gen_pfs_eta,gen_pfs_e,gen_pfs_id,pid):
     #Convert it to a 1D numpy array and perform smearing
     numpy_e_arr = np.asarray(gen_pfs_e.layout.content)
     numpy_pt_arr = np.asarray(gen_pfs_pt.layout.content)
-    numpy_eta_arr = np.asarray(gen_pfs_e.layout.content)
+    numpy_eta_arr = np.asarray(gen_pfs_eta.layout.content)
     numpy_id_arr = np.asarray(gen_pfs_id.layout.content)
 
     if pid == 22:
@@ -83,7 +105,10 @@ def smear_em(gen_pfs_pt,gen_pfs_eta,gen_pfs_e,gen_pfs_id,pid):
 
     # only apply smearing to desired particles matching pid
     smeared_e_arr = np.where(np.abs(numpy_id_arr)==pid,smeared_e_arr,numpy_e_arr)
+    
+
     smeared_pt_arr = np.where(np.abs(numpy_id_arr)==pid,smeared_e_arr/np.cosh(numpy_eta_arr),numpy_pt_arr)
+    #smeared_pt_arr = numpy_pt_arr
 
     #Convert it back to awkward form
     return Array(ListOffsetArray64(gen_pfs_e.layout.offsets, Array(smeared_e_arr).layout)),Array(ListOffsetArray64(gen_pfs_pt.layout.offsets, Array(smeared_pt_arr).layout))
@@ -94,9 +119,20 @@ gen_pfs_e, gen_pfs_pt = smear_em(gen_pfs_pt,gen_pfs_eta,gen_pfs_e,gen_pfs_pid,22
 gen_pfs_e, gen_pfs_pt = smear_em(gen_pfs_pt,gen_pfs_eta,gen_pfs_e,gen_pfs_pid,11)
 gen_pfs_e, gen_pfs_pt = sch.smear_chargedhad(gen_pfs_pt,gen_pfs_eta,gen_pfs_e,gen_pfs_pid,gen_pfs_charge,22)
 
-#print(gen_pfs_pt)
 
 #gen_pfs_e, gen_pfs_pt = sch.smear_hcal(gen_pfs_pt,gen_pfs_eta,gen_pfs_e,gen_pfs_pid,gen_pfs_charge,22)
+
+
+
+def check_wtf(gen_pfs_pid):
+    from awkward import Array
+    from awkward.layout import ListOffsetArray64
+    
+    #Convert it to a 1D numpy array
+    numpy_pid_arr = np.asarray(gen_pfs_pid.layout.content)
+    #print(np.any(numpy_pid_arr == 3))
+    return None
+
 
 def one_hot_pid(gen_pfs_pid,gen_pfs_charge,gen_pfs_eta):
     from awkward import Array
@@ -121,9 +157,15 @@ def one_hot_pid(gen_pfs_pid,gen_pfs_charge,gen_pfs_eta):
     # Give photons 4
     new_pid_arr = np.where((np.abs(new_pid_arr)==22),4.,new_pid_arr)
 
+    #print(np.any(new_pid_arr == 3))
+
     return Array(ListOffsetArray64(gen_pfs_pid.layout.offsets, Array(new_pid_arr).layout))
 
 gen_pfs_pid = one_hot_pid(gen_pfs_pid,gen_pfs_charge,gen_pfs_eta)
+
+check_wtf(gen_pfs_pid)
+
+
 
 
 def disable_charge(gen_pfs_charge,gen_pfs_eta,gen_pfs_genvtx):
@@ -135,21 +177,27 @@ def disable_charge(gen_pfs_charge,gen_pfs_eta,gen_pfs_genvtx):
     numpy_eta_arr = np.asarray(gen_pfs_eta.layout.content)
     numpy_genvtx_arr = np.asarray(gen_pfs_genvtx.layout.content)
 
-    new_charge_arr = np.where(numpy_eta_arr<3.0,numpy_charge_arr,0.)
+    new_charge_arr = np.where(np.abs(numpy_eta_arr)<3.0,numpy_charge_arr,0.)
     new_vtx_arr = np.where(numpy_charge_arr==0,-1,numpy_genvtx_arr)
     return Array(ListOffsetArray64(gen_pfs_charge.layout.offsets, Array(new_charge_arr).layout)),  Array(ListOffsetArray64(gen_pfs_genvtx.layout.offsets, Array(new_vtx_arr).layout))
 
 
 gen_pfs_charge, gen_pfs_vtx = disable_charge(gen_pfs_charge,gen_pfs_eta,gen_pfs_genvtx)
 
+
+check_wtf(gen_pfs_pid)
+
+print(gen_pfs_pt[0])
+
 # kinematics mask
+#kinematics_mask = (gen_pfs_pt>1.0) & (np.abs(gen_pfs_eta)<5.)
 kinematics_mask = (gen_pfs_pt>1.0) & (np.abs(gen_pfs_eta)<5.)
 def filter_minpt(arr):
     newarr = arr[kinematics_mask]
     return newarr
 
-gen_pfs_pt = filter_minpt(gen_pfs_pt)
 
+gen_pfs_pt = filter_minpt(gen_pfs_pt)
 gen_pfs_eta = filter_minpt(gen_pfs_eta)
 gen_pfs_phi = filter_minpt(gen_pfs_phi)
 gen_pfs_e = filter_minpt(gen_pfs_e)
@@ -159,6 +207,9 @@ gen_pfs_z = filter_minpt(gen_pfs_z)
 gen_pfs_vtx = filter_minpt(gen_pfs_vtx)
 gen_pfs_genvtx = filter_minpt(gen_pfs_genvtx)
 
+
+
+check_wtf(gen_pfs_pid)
 
 
 gen_vtx_id = ak.local_index(gen_vtx_z)
@@ -227,6 +278,19 @@ gen_vtx_id = gen_vtx_id[gen_vtx_ndf>0]
 #exit(1)
 
 
+def get_reco_z(gen_pfs_z,gen_pfs_charge):
+    from awkward import Array
+    from awkward.layout import ListOffsetArray64
+    
+    #Convert it to a 1D numpy array
+    numpy_charge_arr = np.asarray(gen_pfs_charge.layout.content)
+    numpy_z_arr = np.asarray(gen_pfs_z.layout.content)
+
+    new_z_arr = np.where(numpy_charge_arr!=0.0,numpy_z_arr,-199.)
+    return Array(ListOffsetArray64(gen_pfs_charge.layout.offsets, Array(new_z_arr).layout))
+
+
+gen_pfs_recoz = get_reco_z(gen_pfs_z,gen_pfs_charge)
 
 final_pfs_pt = np.array(ak.fill_none(ak.pad_none(gen_pfs_pt,7000,clip=True),0,axis=-1))
 final_pfs_eta = np.array(ak.fill_none(ak.pad_none(gen_pfs_eta,7000,clip=True),0,axis=-1))
@@ -235,15 +299,16 @@ final_pfs_e = np.array(ak.fill_none(ak.pad_none(gen_pfs_e,7000,clip=True),0,axis
 final_pfs_pid = np.array(ak.fill_none(ak.pad_none(gen_pfs_pid,7000,clip=True),0,axis=-1))
 final_pfs_charge = np.array(ak.fill_none(ak.pad_none(gen_pfs_charge,7000,clip=True),0,axis=-1))
 final_pfs_z = np.array(ak.fill_none(ak.pad_none(gen_pfs_z,7000,clip=True),0,axis=-1))
+final_pfs_recoz = np.array(ak.fill_none(ak.pad_none(gen_pfs_recoz,7000,clip=True),0,axis=-1))
 final_pfs_vtx = np.array(ak.fill_none(ak.pad_none(gen_pfs_vtx,7000,clip=True),0,axis=-1))
 final_pfs_genvtx = np.array(ak.fill_none(ak.pad_none(gen_pfs_genvtx,7000,clip=True),0,axis=-1))
 
-final_pfs_features = np.stack((final_pfs_pt,final_pfs_eta,final_pfs_phi,final_pfs_e,final_pfs_pid,final_pfs_charge),axis=-1)
+final_pfs_features = np.stack((final_pfs_pt,final_pfs_eta,final_pfs_phi,final_pfs_e,final_pfs_pid,final_pfs_charge,final_pfs_recoz),axis=-1)
 
 #print(final_pfs_vtx)
 #print(final_pfs_vtx.shape)
-print(final_pfs_features)
-print(final_pfs_features.shape)
+#print(final_pfs_features)
+#print(final_pfs_features.shape)
 
 
 final_vtx_x = np.array(ak.fill_none(ak.pad_none(gen_vtx_x,200,clip=True),0,axis=-1))
